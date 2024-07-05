7f48ab49ea04276455c490694c9872e939c77fe8
Tomas Ignacio Salas Arancibia <tsalas@mgcertifica.cl>
2024-07-05 01:34:53 -0400

    fix

--- src/index.js
+++ src/index.js
@@ -30,7 +30,7 @@ app.use((req, res, next) => {
   if (allowedOrigins.includes(origin)) {
     res.header("Access-Control-Allow-Origin", origin);
   }
-  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Credentials", true);
   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
   res.header(
     "Access-Control-Allow-Headers",
