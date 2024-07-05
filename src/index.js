d7f3073a43f1d22e19f6b9eba06ad0bbeb2a92cd
Tomas Ignacio Salas Arancibia <tsalas@mgcertifica.cl>
2024-07-05 01:07:43 -0400

    cabeceras

--- src/index.js
+++ src/index.js
@@ -13,5 +13,18 @@ app.use(
     credentials: true,
   })
 );
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://front-canchas.vercel.app");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(200);
+  }
+  next();
+});
 app.use(routes);
 app.listen(3000);
