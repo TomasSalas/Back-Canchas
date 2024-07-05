2cbb51fbadd82720c61d4ea598bb6335588bc389
Tomas Ignacio Salas Arancibia <tsalas@mgcertifica.cl>
2024-07-05 02:01:40 -0400

    console.log token

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "https://front-canchas.vercel.app",
+    origin: "https://front-canchas-git-main-tomassalas-projects.vercel.app",
     credentials: true,
   })
 );
