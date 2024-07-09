48383b878ab27fe9a09ffc568b9347e421430362
Tomas Ignacio Salas Arancibia <tsalas@mgcertifica.cl>
2024-07-08 22:09:24 -0400

    fix url base

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["https://front-canchas-production.up.railway.app/"],
+    origin: ["https://front-canchas-production.up.railway.app"],
     credentials: true,
   })
 );
