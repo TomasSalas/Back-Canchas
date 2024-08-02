7855c98361c0039f4f5caf568213acf7d822df92
Tomas Ignacio Salas Arancibia <tsalas@mgcertifica.cl>
2024-08-01 23:12:31 -0400

    fix update pago

--- src/Controllers/UpdateTipoPago.js
+++ src/Controllers/UpdateTipoPago.js
@@ -4,20 +4,28 @@ export const UpdateTipoPago = async (req, res) => {
   console.log(req.body)
   const { idArriendo, idPago, pago } = req.body
 
-  const [result] = await pool.query(
-    'UPDATE Reserva SET id_pago = ?, pagada = ? WHERE id_arriendo = ?',
-    [idPago, idArriendo, pago]
-  )
+  try {
+    const [result] = await pool.query(
+      'UPDATE Reserva SET id_pago = ?, pagada = ? WHERE id_arriendo = ?',
+      [idPago, pago, idArriendo]
+    )
 
-  if (result.affectedRows > 0) {
-    return res.json({
-      error: false,
-      data: 'Pago actualizado'
-    })
-  } else {
+    if (result.affectedRows > 0) {
+      return res.json({
+        error: false,
+        data: 'Pago actualizado'
+      })
+    } else {
+      return res.json({
+        error: true,
+        data: 'Error en actualizar el pago'
+      })
+    }
+  } catch (error) {
+    console.error(error)
     return res.json({
       error: true,
-      data: 'Error en actualizar el pago'
+      data: 'Error en la base de datos'
     })
   }
 }
